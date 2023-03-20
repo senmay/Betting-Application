a3e686bb345926d2849139f7736fd1a689538e83
Mateusz Szablak <soulshunter@wp.pl>
2023-03-20 18:35:41 +0100

    aktuator

--- src/main/java/com/dominik/typer/validators/DifferentTeamIds.java
+++ src/main/java/com/dominik/typer/validators/DifferentTeamIds.java
@@ -9,8 +9,16 @@ import java.lang.annotation.*;
 @Constraint(validatedBy = DifferentTeamIdsValidator.class)
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+//@Repeatable(DifferentTeamIds.List.class)
 public @interface DifferentTeamIds {
     String message() default "Home team ID and away team ID must be different.";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
+
+//    @Target({ TYPE })
+//    @Retention(RUNTIME)
+//    @Documented
+//    @interface List {
+//        DifferentTeamIds[] value();
+//    }
 }
