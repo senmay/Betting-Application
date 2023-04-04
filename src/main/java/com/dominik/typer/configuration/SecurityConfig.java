5c151f2d343aa5c3c644f661bb2979951920df63
dominik <dominik.szarzynski@gmail.com>
2023-04-04 21:14:16 +0200

    testing filterChain security

---
+++ src/main/java/com/dominik/typer/configuration/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.dominik.typer.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST,"/bet").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/bet").hasRole("USER")
+                        .requestMatchers("/test/a").hasRole("ADMIN")
+                        .requestMatchers("/test/**").authenticated()
+                        .anyRequest().authenticated())
+                .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        // The builder will ensure the passwords are encoded before saving in memory
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails user = users
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = users
+                .username("admin")
+                .password("password")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+
+
+}
--- src/main/java/com/dominik/typer/controller/BetController.java
+++ src/main/java/com/dominik/typer/controller/BetController.java
@@ -1,7 +1,6 @@
 package com.dominik.typer.controller;
 
 import com.dominik.typer.cachelessons.CacheService;
-import com.dominik.typer.cachelessons.Person;
 import com.dominik.typer.model.json.BetJson;
 import com.dominik.typer.model.mapper.BetMapper;
 import com.dominik.typer.service.betpersistence.BetService;
@@ -9,7 +8,6 @@ import com.dominik.typer.validators.GeneralValidator;
 import com.dominik.typer.validators.ValidationGroupJson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -37,20 +35,5 @@ public class BetController {
     @ResponseStatus(HttpStatus.OK)
     List<BetJson> getBetsFromUser(@RequestHeader("login") String username) {
         return betMapper.mapToListBetJson(betService.getBetsFromUser(username));
-    }
-
-    @GetMapping("/test")
-    int a() {
-        Cache persons = cacheManager.getCache("persons");
-        log.warn("pc-1");
-        Person mat = cacheService.getPersonByName("Mat");
-        log.warn("pc-2");
-        Person mat2 = cacheService.getPersonByName("Mat");
-        System.out.println(mat == mat2);
-        System.out.println(mat.equals(mat2));
-        log.warn("pc-3");
-        Person p3 = cacheService.getPersonByName("Adam");
-        log.warn("pc-4");
-        return 1;
     }
 }
