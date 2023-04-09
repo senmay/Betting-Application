ca4767be7e501a0f87656a8af6eea5425a5e1c52
senmay <dominik.szarzynski@gmail.com>
2023-04-09 16:28:58 +0200

    changing pom

--- src/main/java/com/dominik/typer/configuration/AppConfig.java
+++ src/main/java/com/dominik/typer/configuration/AppConfig.java
@@ -3,11 +3,9 @@ package com.dominik.typer.configuration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
-@PropertySource(value = "classpath:database.properties", ignoreResourceNotFound = true)
 @EnableConfigurationProperties
 @EnableAsync
 @EnableAspectJAutoProxy
