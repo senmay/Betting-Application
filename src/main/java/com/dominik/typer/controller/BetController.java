86218e92c7e50a0cb6d8010787be1f291112d55e
dominik <dominik.szarzynski@gmail.com>
2023-04-14 19:49:26 +0200

    "csv google authenticator"

--- src/main/java/com/dominik/typer/controller/BetController.java
+++ src/main/java/com/dominik/typer/controller/BetController.java
@@ -1,6 +1,5 @@
 package com.dominik.typer.controller;
 
-import com.dominik.typer.cachelessons.CacheService;
 import com.dominik.typer.model.json.BetJson;
 import com.dominik.typer.model.mapper.BetMapper;
 import com.dominik.typer.service.betpersistence.BetService;
@@ -8,7 +7,6 @@ import com.dominik.typer.validators.GeneralValidator;
 import com.dominik.typer.validators.ValidationGroupJson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.CacheManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,9 +20,6 @@ public class BetController {
     private final BetService betService;
     private final BetMapper betMapper;
     private final GeneralValidator validator;
-    private final CacheService cacheService;
-    private final CacheManager cacheManager;
-
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     void createBet(@RequestBody BetJson betJson) {
