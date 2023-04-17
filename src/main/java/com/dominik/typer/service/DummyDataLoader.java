4ce4b94f96ef6bcff925bfc00de101a101766371
dominik <dominik.szarzynski@gmail.com>
2023-04-17 18:19:58 +0200

    fixing parse matches

--- src/main/java/com/dominik/typer/service/DummyDataLoader.java
+++ src/main/java/com/dominik/typer/service/DummyDataLoader.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Profile("dummy")
@@ -32,14 +33,18 @@ public class DummyDataLoader {
         User user = User.builder().id(2).username("user").password(passwordEncoder.encode("dominiczek")).email("user.wp.pl").points(10).balance(100.00).build();
         userPersistence.saveAdmin(admin);
         userPersistence.saveWithAdmin(user);
-        Team legia = Team.builder().id(1).name("legia").inactive(false).build();
-        Team wisla = Team.builder().id(2).name("wisla").inactive(false).build();
+        Team chelsea = Team.builder().id(1).name("Chelsea").inactive(false).build();
+        Team real = Team.builder().id(2).name("Real Madrid").inactive(false).build();
         Team milan = Team.builder().id(3).name("AC Milan").inactive(false).build();
         Team napoli = Team.builder().id(4).name("Napoli").inactive(false).build();
-        teamPersistence.saveTeam(legia);
-        teamPersistence.saveTeam(wisla);
-        teamPersistence.saveTeam(milan);
-        teamPersistence.saveTeam(napoli);
+        Team bayern = Team.builder().id(5).name("Bayern München").inactive(false).build();
+        Team city = Team.builder().id(6).name("Manchester City").inactive(false).build();
+        Team benfica = Team.builder().id(7).name("Benfica").inactive(false).build();
+        Team inter = Team.builder().id(8).name("Internazionale Milano").inactive(false).build();
+        List<Team> list = List.of(chelsea, real, milan, napoli, bayern, city, benfica, inter);
+        for (Team team : list) {
+            teamPersistence.saveTeam(team);
+        }
         Match match1 = Match.builder().id(1).homeTeamId(1)
                 .awayTeamId(2).oddsForAwayTeam(2.00).oddsForHomeTeam(3.00)
                 .oddsForDraw(1.00).dateOfEvent(LocalDateTime.now().plusDays(1))
